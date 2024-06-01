1b2917c3eee2873a74d199c384ab8a48bbcfcd02
伊欧 <langyo.china@gmail.com>
2024-06-01 12:49:13 +0800

    📝 试玩过 kv 之后，再试玩一下 d1。

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use axum::{routing::get, Router};
 use kv::ToRawKvValue;
 use tower_service::Service;
+use wasm_bindgen::JsValue;
 use worker::*;
 
 fn router() -> Router {
@@ -17,20 +18,28 @@ async fn fetch(
     console_error_panic_hook::set_once();
     // Ok(router().call(req).await?)
 
-    let val = env
-        .kv("axumtest")?
-        .get("abc")
-        .text()
-        .await?
-        .map(|v| v.parse().unwrap_or(0))
-        .unwrap_or(0);
-    let ret = format!("ljyys! Request {} times", val);
+    // let val = env
+    //     .kv("axumtest")?
+    //     .get("abc")
+    //     .text()
+    //     .await?
+    //     .map(|v| v.parse().unwrap_or(0))
+    //     .unwrap_or(0);
+    // let ret = format!("ljyys! Request {} times", val);
 
-    let val = val + 1;
-    env.kv("axumtest")?
-        .put("abc", val.to_string())?
-        .execute()
+    // let val = val + 1;
+    // env.kv("axumtest")?
+    //     .put("abc", val.to_string())?
+    //     .execute()
+    //     .await?;
+
+    let val = env
+        .d1("d1")?
+        .prepare(r#"SELECT * FROM user WHERE id = ?;"#)
+        .bind(&[2.into()])?
+        .run()
         .await?;
+    let ret = format!("ljyys! list: {:?}", val.results::<serde_json::Value>()?);
 
     Ok(axum::http::Response::new(ret.into()))
 }
